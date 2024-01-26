5dea6d29677bb7cd814c0050e65f9d961b87ddff
Rahil <80545477+Rahil2892@users.noreply.github.com>
2024-01-26 17:04:23 +0000

    Timer Bug Fix

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import {useRef} from 'react';
 import { io } from "socket.io-client";
 import { socket } from "../socket";
+import { useCountdown } from "../hooks/useCountDown";
 import MessageBox from "../Components/MessageBox";
 import { db } from "../firebase-config";
 import PlayerList from "../Components/PlayerList";
 import CharList from "../Components/CharList";
-import CountdownTimer from "../Components/CountDownTimer";
+import DateTimeDisplay from "../Components/DateTimeDisplay";
 import {
   collection,
   getDocs,
@@ -19,6 +20,7 @@ import {
   query,
   where,
 } from "firebase/firestore";
+
 
 // const socket = io("http://localhost:3001");
 
@@ -38,11 +40,15 @@ const GameRoom = () => {
   const [playerList, setPlayerList] = useState([]);
   const [roundNum, setRoundNum] = useState(1);
   const [selector, setSelector] = useState();
+  
+    
+  const [minutes, seconds] = useCountdown(dateTimeAfterTwoMins);
 
   const setChar = async (charName) => {
     await updateDoc(doc(db, "rooms", inputRoom, inputRoom, userName), {
       selectedChar: charName,
     });
+    
     setSelectedChar(charName);
     currentSocket.emit("selected-char", charName, inputRoom);
     console.log("setSelectedChar");
@@ -122,6 +128,62 @@ const GameRoom = () => {
     await deleteDoc(q);
   };
 
+  const ExpiredNotice = () => {
+    return (
+      <div className="expired-notice">
+        <span>Expired!!!</span>
+        <p>Please select a future date and time.</p>
+      </div>
+    );
+  };
+
+  const ShowCounter = ({ minutes, seconds, selector }) => {
+    const [inputRoom, setInputRoom] = useState(sessionStorage.getItem("room"));
+    const [userName, setUserName] = useState(sessionStorage.getItem("username"));
+    const [guessedChar, setGuessedChar] = useState();
+  
+    const setGuess = async (charName) => {
+      await updateDoc(doc(db, "rooms", inputRoom, inputRoom, userName), {
+        guessChar: charName,
+      });
+      setGuessedChar(charName);
+      // socket.emit("selected-char",charName,inputRoom);
+    };
+  
+    return (
+      <div className="show-counter">
+        <a
+          href="https://tapasadhikary.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="countdown-link"
+        >
+          <DateTimeDisplay
+            value={minutes}
+            type={"Mins"}
+            isDanger={seconds <= 10 && minutes == 0}
+          />
+          <p>:</p>
+          <DateTimeDisplay
+            value={seconds}
+            type={"Seconds"}
+            isDanger={seconds <= 10 && minutes == 0}
+          />
+        </a>
+        <br />
+        <div>
+          {selector != userName ? (
+            <div>
+              <CharList setChar={setGuess} />
+            </div>
+          ) : (
+            <div>you are selector</div>
+          )}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <h1>Game Room : {inputRoom}</h1>
@@ -186,10 +248,9 @@ const GameRoom = () => {
 
     {selectedChar ? (
             <div>
-              <CountdownTimer
-                targetDate={dateTimeAfterTwoMins}
-                selector={selector}
-              />
+               {(minutes + seconds <= 0)? 
+               (<div><ExpiredNotice /></div>) :(<div><ShowCounter minutes={minutes} seconds={seconds} selector={selector} /></div>) 
+      }
             </div>
           ) : (
             <div>Wait till selectioin of the character</div>
