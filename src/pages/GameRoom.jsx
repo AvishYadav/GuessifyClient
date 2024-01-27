7df36c09eff0fb52e65e9badadd7907965a891a8
Rahil <80545477+Rahil2892@users.noreply.github.com>
2024-01-27 11:46:25 +0000

    Timer Bug Resolved

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {useRef} from 'react';
 import { io } from "socket.io-client";
 import { socket } from "../socket";
-import { useCountdown } from "../hooks/useCountDown";
 import MessageBox from "../Components/MessageBox";
 import { db } from "../firebase-config";
 import PlayerList from "../Components/PlayerList";
 import CharList from "../Components/CharList";
-import DateTimeDisplay from "../Components/DateTimeDisplay";
+import CountdownTimer from "../Components/CountDownTimer";
 import {
   collection,
   getDocs,
@@ -21,12 +19,10 @@ import {
   where,
 } from "firebase/firestore";
 
-
 // const socket = io("http://localhost:3001");
 
 const GameRoom = () => {
   const [currentSocket, setCurrentSocket] = useState(socket);
-  const inputRef = useRef(null);
   const [inputMsg, setInputMsg] = useState("");
   const [inputRoom, setInputRoom] = useState(sessionStorage.getItem("room"));
   const TWO_MIN_IN_MS = 20 * 60 * 1000;
@@ -40,15 +36,11 @@ const GameRoom = () => {
   const [playerList, setPlayerList] = useState([]);
   const [roundNum, setRoundNum] = useState(1);
   const [selector, setSelector] = useState();
-  
-    
-  const [minutes, seconds] = useCountdown(dateTimeAfterTwoMins);
 
   const setChar = async (charName) => {
     await updateDoc(doc(db, "rooms", inputRoom, inputRoom, userName), {
       selectedChar: charName,
     });
-    
     setSelectedChar(charName);
     currentSocket.emit("selected-char", charName, inputRoom);
     console.log("setSelectedChar");
@@ -128,62 +120,6 @@ const GameRoom = () => {
     await deleteDoc(q);
   };
 
-  const ExpiredNotice = () => {
-    return (
-      <div className="expired-notice">
-        <span>Expired!!!</span>
-        <p>Please select a future date and time.</p>
-      </div>
-    );
-  };
-
-  const ShowCounter = ({ minutes, seconds, selector }) => {
-    const [inputRoom, setInputRoom] = useState(sessionStorage.getItem("room"));
-    const [userName, setUserName] = useState(sessionStorage.getItem("username"));
-    const [guessedChar, setGuessedChar] = useState();
-  
-    const setGuess = async (charName) => {
-      await updateDoc(doc(db, "rooms", inputRoom, inputRoom, userName), {
-        guessChar: charName,
-      });
-      setGuessedChar(charName);
-      // socket.emit("selected-char",charName,inputRoom);
-    };
-  
-    return (
-      <div className="show-counter">
-        <a
-          href="https://tapasadhikary.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="countdown-link"
-        >
-          <DateTimeDisplay
-            value={minutes}
-            type={"Mins"}
-            isDanger={seconds <= 10 && minutes == 0}
-          />
-          <p>:</p>
-          <DateTimeDisplay
-            value={seconds}
-            type={"Seconds"}
-            isDanger={seconds <= 10 && minutes == 0}
-          />
-        </a>
-        <br />
-        <div>
-          {selector != userName ? (
-            <div>
-              <CharList setChar={setGuess} />
-            </div>
-          ) : (
-            <div>you are selector</div>
-          )}
-        </div>
-      </div>
-    );
-  };
-
   return (
     <>
       <h1>Game Room : {inputRoom}</h1>
@@ -212,16 +148,16 @@ const GameRoom = () => {
         >
           <MessageBox msgs={msgs} />
           <input
-            ref={inputRef}
             type="text"
             placeholder="say something"
             id="message-input"
+            onChange={(e) => setInputMsg(e.target.value)}
           ></input>
           <button
             style={{ width: "70px", height: "30px" }}
             type="button"
             id="message-button"
-            onClick={() => sendMsg(inputRef.current.value, inputRoom)}
+            onClick={() => sendMsg(inputMsg, inputRoom)}
           >
             Send
           </button>
@@ -236,22 +172,22 @@ const GameRoom = () => {
             flex: "20%",
           }}
         >
+          {selectedChar ? (
+            <div>
+              <CountdownTimer
+                targetDate={dateTimeAfterTwoMins}
+                selector={selector}
+              />
+            </div>
+          ) : (
+            <div>Wait till selectioin of the character</div>
+          )}
           {selector == userName && !selectedChar ? (
             <div>
               <CharList setChar={setChar} />
             </div>
           ) : selectedChar ? (
             <div>Character is selected</div>
-          ) : (
-            <div>Wait till selectioin of the character</div>
-          )}
-
-    {selectedChar ? (
-            <div>
-               {(minutes + seconds <= 0)? 
-               (<div><ExpiredNotice /></div>) :(<div><ShowCounter minutes={minutes} seconds={seconds} selector={selector} /></div>) 
-      }
-            </div>
           ) : (
             <div>Wait till selectioin of the character</div>
           )}
